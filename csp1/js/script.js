9a7ba21bd90a6240741cd4debfcd1264c839f7f4
andromarces <andromarces@gmail.com>
2017-12-12 16:52:27 +0800

    updated csp1

--- csp1/js/script.js
+++ csp1/js/script.js
@@ -18,3 +18,23 @@ $(function () {
 		}
 	})
 })
+
+/* back to top script */
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+		document.getElementById("topBtn").style.display = "inline-block";
+	} else {
+		document.getElementById("topBtn").style.display = "none";
+	}
+}
+
+$(function () {
+	'use strict'
+
+	$('[data-toggle="top"]').on('click', function () {
+		document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+})
+})
