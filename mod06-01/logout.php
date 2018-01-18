ad05f0a82db87b378fbe59874e62d4a00cf35d38
andromarces <andromarces@gmail.com>
2018-01-18 16:50:18 +0800

    update items.php in mod04-03

--- mod06-01/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-session_destroy();
-
-?>
-
-Log out Successful.
--- mod06-01/result.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-// echo "username: ".$_POST['username'];
-
-echo $_SESSION['username'];
-
-?>
