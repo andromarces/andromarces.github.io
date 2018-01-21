af642daa432658181b7b5b9f815c3290cb184a1c
andromarces <andromarces@gmail.com>
2018-01-21 21:55:46 +0800

    updated template.php in csp2: tweaked navbars

--- csp2/assets/js/template.js
+++ csp2/assets/js/template.js
@@ -1,6 +1,7 @@
-$(function () {
-    "use strict"
+$(function () { /* document ready function */
+    "use strict" /* strict mode enabled */
 
+    // function to close menus when user clicks outside of them
     function ddbtnclose(event, element, menu) {
         if (!$(event.target).closest(element).length) {
             if ($(menu).css("display") == "block") {
@@ -9,69 +10,73 @@ $(function () {
         }
     }
 
+    // toggle to close or open .ddmenu
     $("#navbarDropdownMenuLink").click(function () {
         $(".ddmenu").fadeToggle(350);
     });
 
+    // simulate clicking on #navbarDropdownMenuLink to open .ddmenu when mouse hovers on #navbarDropdownMenuLink
     $("#navbarDropdownMenuLink").mouseenter(function () {
         if ($(".ddmenu").css("display") !== "block" && $("span.d-none").css("display") !== "none") {
             $("#navbarDropdownMenuLink").click();
         }
     });
 
-    $(".ddmenu").mouseleave(function () {
-        if ($(".ddmenu").css("display") == "block" && $("span.d-none").css("display") !== "none") {
-            $(".ddmenu").fadeOut(350);
-        }
-    });
-
+    // close .ddmenu when mouse leaves top navbar
     $("nav").mouseleave(function () {
         if ($(".ddmenu").css("display") == "block" && $("span.d-none").css("display") !== "none") {
             $(".ddmenu").fadeOut(350);
         }
     });
 
+    // toggle to close or open .ddsu1
     $("#dropdownMenuButton1").click(function () {
         $(".ddsu1").fadeToggle(350);
     });
 
+    // toggle to close or open .ddsu2
     $("#dropdownMenuButton2").click(function () {
         $(".ddsu2").fadeToggle(350);
     });
 
+    // call ddbtnclose to close .ddmenu when user clicks outside of #ddp
     $(document).click(function () {
         ddbtnclose(event, document.getElementById("ddp"), document.getElementsByClassName("ddmenu"));
     });
 
+    // call ddbtnclose to close .ddsu1 when user clicks outside of #ddsu1
     $(document).click(function () {
         ddbtnclose(event, document.getElementById("ddsu1"), document.getElementsByClassName("ddsu1"));
     });
 
+    // call ddbtnclose to close .ddsu2 when user clicks outside of #ddsu2
     $(document).click(function () {
         ddbtnclose(event, document.getElementById("ddsu2"), document.getElementsByClassName("ddsu2"));
     });
 
+    // toggle to close or open div.navbar-collapse
     $("button.navbar-toggler").click(function () {
-        if ($("div.navbar-collapse").css("left") == "-100px") {
+        if ($("div.navbar-collapse").css("left") == "-210px") {
             $("div.navbar-collapse").animate({
-                left: 5
+                left: 1
             }, 350, function () {
                 $("button.navbar-toggler").attr("aria-expanded", "true");
             });
         } else {
             $("div.navbar-collapse").animate({
-                left: -100
+                left: -210
             }, 350, function () {
                 $("button.navbar-toggler").attr("aria-expanded", "false");
             });
         }
     });
 
+    // close div.navbar-collapse when clicking outside #navbarNavDropdown
     $(document).click(function () {
         if (!$(event.target).closest($('#navbarNavDropdown')).length) {
-            if ($("div.navbar-collapse").css("left") == "5px") {
+            if ($("div.navbar-collapse").css("left") == "1px") {
                 $("div.navbar-collapse").animate({
-                    left: -100
+                    left: -210
                 }, 350, function () {
                     $("button.navbar-toggler").attr("aria-expanded", "false");
                 });
--- csp2/template.php
+++ csp2/template.php
@@ -9,7 +9,10 @@
     <title>
         <?php //display_title();?>
     </title>
+
+    <!-- imports Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Audiowide|Open+Sans" rel="stylesheet">
+
     <!-- ****** faviconit.com favicons ****** -->
     <link rel="shortcut icon" href="assets/img/favicon/Pinoyware.ico">
     <link rel="icon" sizes="16x16 32x32 64x64" href="assets/img/favicon/Pinoyware.ico">
@@ -32,90 +35,47 @@
     <meta name="msapplication-TileImage" content="assets/img/favicon/Pinoyware-144.png">
     <meta name="msapplication-config" content="assets/img/favicon/browserconfig.xml">
     <!-- ****** faviconit.com favicons ****** -->
+
+    <!-- imports Bootstrap 4.0.0 css -->
     <link rel="stylesheet" href="assets/lib/bootstrap.css">
+
+    <!-- imports FontAwesome 5.0.4 js -->
     <script defer src="assets/lib/fontawesome-5.0.4.js"></script>
+
+    <!-- imports template.css -->
     <link rel="stylesheet" href="assets/css/template.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-md navbar-light bg-white pt-1" id="navBar">
-        <button class="navbar-toggler" type="button" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="dropdown d-md-none ml-auto mr-1" id="ddsu1">
-            <button class="btn btn-light bg-white" type="button" id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-sign-in-alt"></i> Login
+    <!-- .container-fluid / whole body container -->
+    <div class="container-fluid">
+        <!-- #navBar / top navbar -->
+        <nav class="row navbar navbar-expand-md navbar-light bg-white pt-1 pb-0" id="navBar">
+            <!-- .navbar-toggler dropdown button when top navbar collapses -->
+            <button class="navbar-toggler" type="button" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="dropdown-menu dropdown-menu-right ddsu1">
-                <form class="px-4 py-3">
-                    <div class="form-group">
-                        <label for="DropdownFormEmail1">Email Address</label>
-                        <input type="email" class="form-control" id="DropdownFormEmail1" placeholder="Email" autocomplete="username email">
-                    </div>
-                    <div class="form-group">
-                        <label for="DropdownFormPassword1">Password</label>
-                        <input type="password" class="form-control" id="DropdownFormPassword1" placeholder="Password" autocomplete="current-password">
-                    </div>
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="dropdownCheck1">
-                        <label class="form-check-label" for="dropdownCheck1">
-                            Remember Me
-                        </label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </form>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">New around here? Sign up</a>
-                <a class="dropdown-item" href="#">Forgot password?</a>
-            </div>
-        </div>
-        <a class="navbar-brand font-weight-bold mr-0" href="#">
-            <img src="/csp2/assets/img/1.png" width="30" height="30" class="d-inline-block align-top" alt="">
-            <span class="d-none d-md-inline-block">Pinoyware</span>
-        </a>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
-                        <span class="sr-only">(current)</span>
-                        <!-- JS IF THIS! -->
-                    </a>
-                </li>
-                <li class="nav-item dropdown" id="ddp">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Products
-                    </a>
-                    <div class="dropdown-menu ddmenu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">PCs</a>
-                        <a class="dropdown-item" href="#">Laptops</a>
-                        <a class="dropdown-item" href="#">Accessories</a>
-                        <a class="dropdown-item" href="#">All Products</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact Us</a>
-                </li>
-            </ul>
-            <div class="dropdown ml-2 ml-auto" id="ddsu2">
-                <button class="btn btn-light bg-white" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-sign-in-alt"></i> Login
+            <!-- #ddsu1 / dropdown for login/sign-up 1 (ddsu1) -->
+            <div class="dropdown d-md-none ml-auto mr-1" id="ddsu1">
+                <!-- #dropdownMenyButton1 / dropdown button for .ddsu1 -->
+                <button class="btn btn-light bg-white" type="button" id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    <i class="fas fa-sign-in-alt"></i> Login / Sign Up
                 </button>
-                <div class="dropdown-menu dropdown-menu-right ddsu2">
+                <!-- .ddsu1 / dropdown menu login/sign-up 1 -->
+                <div class="dropdown-menu dropdown-menu-right ddsu1">
                     <form class="px-4 py-3">
                         <div class="form-group">
-                            <label for="DropdownFormEmail2">Email Address</label>
-                            <input type="email" class="form-control" id="DropdownFormEmail2" placeholder="Email" autocomplete="username email">
+                            <label for="DropdownFormEmail1">Email Address</label>
+                            <input type="email" class="form-control" id="DropdownFormEmail1" placeholder="Email" autocomplete="username email">
                         </div>
                         <div class="form-group">
-                            <label for="DropdownFormPassword2">Password</label>
-                            <input type="password" class="form-control" id="DropdownFormPassword2" placeholder="Password" autocomplete="current-password">
+                            <label for="DropdownFormPassword1">Password</label>
+                            <input type="password" class="form-control" id="DropdownFormPassword1" placeholder="Password" autocomplete="current-password">
                         </div>
                         <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="dropdownCheck2">
-                            <label class="form-check-label" for="dropdownCheck2">
+                            <input type="checkbox" class="form-check-input" id="dropdownCheck1">
+                            <label class="form-check-label" for="dropdownCheck1">
                                 Remember Me
                             </label>
                         </div>
@@ -125,9 +85,111 @@
                     <a class="dropdown-item" href="#">New around here? Sign up</a>
                     <a class="dropdown-item" href="#">Forgot password?</a>
                 </div>
+                <!-- /.ddsu1 -->
             </div>
-        </div>
-    </nav>
+            <!-- /#ddsu1 -->
+
+            <!-- .navbar-brand -->
+            <a class="navbar-brand font-weight-bold mr-0" href="#">
+                <img src="/csp2/assets/img/1.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                <span class="d-none d-md-inline-block">Pinoyware</span>
+            </a>
+            <!-- #navbarNavDropdown / dropdown menu when top navbar collapses -->
+            <div class="collapse navbar-collapse px-3 px-md-0" id="navbarNavDropdown">
+                <!-- .navbar-nav / contains links for Home, Products, About, Contact Us -->
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home
+                            <span class="sr-only">(current)</span>
+                            <!-- JS IF THIS! -->
+                        </a>
+                    </li>
+                    <!-- #ddp / dropdown for Products -->
+                    <li class="nav-item dropdown" id="ddp">
+                        <!-- #navbarDropdownMenuLink / dropdown button for .ddmenu -->
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Products
+                        </a>
+                        <!-- .ddmenu / dropdown menu for Products -->
+                        <div class="dropdown-menu ddmenu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">PCs</a>
+                            <a class="dropdown-item" href="#">Laptops</a>
+                            <a class="dropdown-item" href="#">Accessories</a>
+                            <a class="dropdown-item" href="#">All Products</a>
+                        </div>
+                        <!-- /.ddmenu -->
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <!-- /.navbar-nav -->
+
+                <div class="dropdown-divider"></div>
+
+                <!-- #ddsu2 / dropdown login/sign-up 2 (ddsu2) -->
+                <div class="dropdown ml-2 ml-auto" id="ddsu2">
+                    <!-- #.dropdownMenuButton2 / dropdown button for .ddsu2 -->
+                    <button class="btn btn-light bg-white px-0" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
+                        <i class="fas fa-sign-in-alt"></i> Login / Sign Up
+                    </button>
+                    <!-- .ddsu2 / dropdown menu login/sign-up 2 -->
+                    <div class="dropdown-menu dropdown-menu-right ddsu2">
+                        <form class="px-4 py-3">
+                            <div class="form-group">
+                                <label for="DropdownFormEmail2">Email Address</label>
+                                <input type="email" class="form-control" id="DropdownFormEmail2" placeholder="Email" autocomplete="username email">
+                            </div>
+                            <div class="form-group">
+                                <label for="DropdownFormPassword2">Password</label>
+                                <input type="password" class="form-control" id="DropdownFormPassword2" placeholder="Password" autocomplete="current-password">
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="dropdownCheck2">
+                                <label class="form-check-label" for="dropdownCheck2">
+                                    Remember Me
+                                </label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </form>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">New around here? Sign up</a>
+                        <a class="dropdown-item" href="#">Forgot password?</a>
+                    </div>
+                    <!-- /.ddsu2 -->
+                </div>
+                <!-- /#ddsu2 -->
+            </div>
+            <!-- /#navbarNavDropdown -->
+        </nav>
+        <!-- /#navBar // top navbar -->
+
+        <!-- .nav / bottom nav -->
+        <nav class="nav my-1 justify-content-end">
+            <!-- cart button -->
+            <button type="button" class="nav-item btn btn-dark">
+                <i class="fas fa-cart-arrow-down"> </i>
+                <span class="d-none d-md-inline-block"> Cart</span>
+            </button>
+
+            <!-- search form -->
+            <div class="nav-item col-auto px-0">
+                <button type="button" class="btn btn-dark d-md-none"><i class="fas fa-search"></i></button>
+                <form class="form-inline justify-content-end">
+                    <input class="form-control col-8 col-sm-9 col-md-7 col-lg-7" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-dark col-3 col-sm-2 col-md-4 col-lg-4" type="submit">
+                        <i class="fas fa-search d-inline-block d-md-none"></i>
+                        <span class="d-none d-md-inline-block">Search
+                            <span>
+                    </button>
+                </form>
+            </div>
+        </nav>
+        <!-- /.nav // bottom nav -->
 
 
 
@@ -135,12 +197,23 @@
 
 
 
-    <footer class="bg-white text-dark position-relative text-center">
-        <a class="footerlink" href="#">&copy; 2018 Andro O. Marces</a>
-    </footer>
+        <!-- footer -->
+        <footer class="text-dark position-relative row justify-content-center">
+            <a class="footerlink" href="#">&copy; 2018 Andro O. Marces</a>
+        </footer>
+    </div>
+    <!-- /.container-fluid -->
+
+    <!-- imports JQuery 3.2.1 js -->
     <script src="assets/lib/jquery-3.2.1.min.js"></script>
+
+    <!-- imports Popper 1.12.9 js -->
     <script src="assets/lib/popper.min.js"></script>
+
+    <!-- imports Bootstrap 4.0.0 js -->
     <script src="assets/lib/bootstrap.js"></script>
+
+    <!-- imports template.js -->
     <script src="assets/js/template.js"></script>
 </body>
 
