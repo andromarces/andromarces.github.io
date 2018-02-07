86227996908465cc740cf755fa8bd37b106ab312
andromarces <andromarces@gmail.com>
2018-02-07 16:31:27 +0800

    created mod10-02

--- csp1/js/contact-script.js
+++ csp1/js/contact-script.js
@@ -1,55 +1,56 @@
-/* offcanvas script */
+'use strict'
+
 $(function () {
-	'use strict'
+	/* offcanvas script */
 
 	$('[data-toggle="offcanvas"]').on('click', function () {
-		$('.sidebar-offcanvas').toggleClass('active')
-	})
-})
+		$('.sidebar-offcanvas').toggleClass('active');
+	});
 
-$(function () {
-	'use strict'
 
 	$(document).on('click', function (event) {
-		if(!$(event.target).closest('#sidebar').length) {
-			if($('.sidebar-offcanvas').hasClass('active')){
-				$('.sidebar-offcanvas').toggleClass('active')
+		if (!$(event.target).closest('#sidebar').length) {
+			if ($('.sidebar-offcanvas').hasClass('active')) {
+				$('.sidebar-offcanvas').toggleClass('active');
 			}
 		}
-	})
-})
+	});
+	/* back to top script */
+	window.onscroll = function () {
+		scrollFunction();
+	};
 
-/* back to top script */
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-		document.getElementById("topBtn").style.display = "inline-block";
-	} else {
-		document.getElementById("topBtn").style.display = "none";
+	function scrollFunction() {
+		if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+			document.getElementById("topBtn").style.display = "inline-block";
+		} else {
+			document.getElementById("topBtn").style.display = "none";
+		}
 	}
-}
-
-$(function () {
-	'use strict'
 
 	$('[data-toggle="top"]').on('click', function () {
 		document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-})
-})
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	});
+	
+	function initMap() {
+		var purrfect = {
+			lat: 14.632946,
+			lng: 121.043719
+		};
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 17,
+			center: purrfect
+		});
+		var marker = new google.maps.Marker({
+			position: purrfect,
+			map: map
+		});
+		google.maps.event.addDomListener(window, 'resize', function () {
+			map.setCenter(purrfect);
+		});
+	}
+});
 
-function initMap() {
-					var purrfect = {lat: 14.632946, lng: 121.043719};
-					var map = new google.maps.Map(document.getElementById('map'), {
-						zoom: 17,
-						center: purrfect
-					});
-					var marker = new google.maps.Marker({
-						position: purrfect,
-						map: map
-					});
-					google.maps.event.addDomListener(window, 'resize', function() {
-						map.setCenter(purrfect);
-					});
-				}
+
+
--- csp1/js/script.js
+++ csp1/js/script.js
@@ -1,40 +1,35 @@
-/* offcanvas script */
-$(function () {
-	'use strict'
+'use strict'
 
+$(function () {
+
+	/* offcanvas script */
 	$('[data-toggle="offcanvas"]').on('click', function () {
-		$('.sidebar-offcanvas').toggleClass('active')
-	})
-})
-
-$(function () {
-	'use strict'
+		$('.sidebar-offcanvas').toggleClass('active');
+	});
 
 	$(document).on('click', function (event) {
-		if(!$(event.target).closest('#sidebar').length) {
-			if($('.sidebar-offcanvas').hasClass('active')){
-				$('.sidebar-offcanvas').toggleClass('active')
+		if (!$(event.target).closest('#sidebar').length) {
+			if ($('.sidebar-offcanvas').hasClass('active')) {
+				$('.sidebar-offcanvas').toggleClass('active');
 			}
 		}
-	})
-})
+	});
 
-/* back to top script */
-window.onscroll = function() {scrollFunction()};
+	/* back to top script */
+	window.onscroll = function () {
+		scrollFunction()
+	};
 
-function scrollFunction() {
-	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-		document.getElementById("topBtn").style.display = "inline-block";
-	} else {
-		document.getElementById("topBtn").style.display = "none";
+	function scrollFunction() {
+		if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+			document.getElementById("topBtn").style.display = "inline-block";
+		} else {
+			document.getElementById("topBtn").style.display = "none";
+		}
 	}
-}
-
-$(function () {
-	'use strict'
 
 	$('[data-toggle="top"]').on('click', function () {
 		document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-})
-})
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	});
+});
