dba5c3b6ecc9021fc061c3a76726c7c76bc5f464
andromarces <andromarces@gmail.com>
2018-02-22 20:31:12 +0800

    mod00-01: revised portfolio page

---
+++ mod00-01/js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+$(function () {
+
+    // fadeout page loader
+    $(".loader").fadeOut(function () {
+        // fadein landing page
+        $(".landing").fadeIn();
+    });
+
+});
