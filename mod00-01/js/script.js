f938b5f7c493442e743f43d8bd395b88e2940df8
andromarces <andromarces@gmail.com>
2018-02-24 14:08:22 +0800

    mod00-01: added portfolio1 page

--- mod00-01/js/script.js
+++ mod00-01/js/script.js
@@ -16,12 +16,14 @@ $(function () {
     var contentHeight = (window.innerHeight - ($("footer").outerHeight(true) + ($(".landing").outerHeight(true) - $(".landing").innerHeight() + 1)));
     // $(".landing").css("min-height", contentHeight + "px");
     $(".landing").css("min-height", window.innerHeight + "px");
+    $(".portfolio1").css("min-height", window.innerHeight + "px");
 
     // on resize, stick footer to the bottom of viewport when content is too short
     $(window).resize(function () {
         contentHeight = (window.innerHeight - ($("footer").outerHeight(true) + ($(".landing").outerHeight(true) - $(".landing").innerHeight() + 1)));
         // $(".landing").css("min-height", contentHeight + "px");
         $(".landing").css("min-height", window.innerHeight + "px");
+        $(".portfolio1").css("min-height", window.innerHeight + "px");
     });
 
     // animated hamburger icon
@@ -62,14 +64,80 @@ $(function () {
         }
     });
 
+    var bottom = 0;
+    // detect if user is at the bottom of page on page load
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        bottom = 1;
+    }
+
+    // detect if user has scrolled to the bottom
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            bottom = 1;
+        }
+    });
+
+    var top = 0;
+    // detect if user is at the top of page on page load
+    if ($(window).scrollTop() == 0) {
+        top = 1;
+    }
+
+    // detect if user has scrolled to the top
+    $(window).scroll(function () {
+        if ($(window).scrollTop() == 0) {
+            top = 1;
+        }
+    });
+
+    // detect mousescroll
     var scrollDetect = 0;
     $(window).bind("mousewheel DOMMouseScroll", function (event) {
-        if (scrollDetect == 0) {
+        if (scrollDetect == 0 && page > 0) {
             if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-                scrollDetect = 1;
-                
+                if (page > 1 && top == 1) {
+                    scrollDetect = 1;
+                    if (page == 2) {
+                        $("footer").fadeOut();
+                    }
+                    $(".page" + page).fadeOut(function () {
+                        page--;
+                        $(".page" + page).fadeIn(function () {
+                            scrollDetect = 0;
+                            bottom = 0;
+                            top = 0;
+                            if ($(window).scrollTop() == 0) {
+                                top = 1;
+                            }
+                            setTimeout(() => {
+                                if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+                                    bottom = 1;
+                                }
+                            }, 500);
+                        });
+                    });
+                }
             } else {
-                console.log("down");
+                if (page < 3 && bottom == 1) {
+                    scrollDetect = 1;
+                    $(".page" + page).fadeOut(function () {
+                        page++;
+                        if (page == 3) {
+                            $("footer").fadeIn();
+                        }
+                        $(".page" + page).fadeIn(function () {
+                            scrollDetect = 0;
+                            bottom = 0;
+                            top = 0;
+                            if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+                                bottom = 1;
+                            }
+                            if ($(window).scrollTop() == 0) {
+                                top = 1;
+                            }
+                        });
+                    });
+                }
             }
         }
     });
